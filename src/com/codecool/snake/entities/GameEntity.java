e7bb297498d6c602e83a281b7c9deaee06e87edc
matixk <matixk73@gmail.com>
2018-08-22 11:02:04 +0200

    Simplified if statement

--- src/com/codecool/snake/entities/GameEntity.java
+++ src/com/codecool/snake/entities/GameEntity.java
@@ -4,14 +4,12 @@ import com.codecool.snake.Globals;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
-// The base class for every game entity.
 public abstract class GameEntity extends ImageView {
 
     protected Pane pane;
 
     protected GameEntity(Pane pane) {
         this.pane = pane;
-        // add to the main loop.
         Globals.addGameObject(this);
     }
 
@@ -23,10 +21,7 @@ public abstract class GameEntity extends ImageView {
     }
 
     protected boolean isOutOfBounds() {
-        if (getX() > Globals.WINDOW_WIDTH || getX() < 0 ||
-            getY() > Globals.WINDOW_HEIGHT || getY() < 0) {
-            return true;
-        }
-        return false;
+        return getX() > Globals.WINDOW_WIDTH || getX() < 0 ||
+            getY() > Globals.WINDOW_HEIGHT || getY() < 0;
     }
 }
